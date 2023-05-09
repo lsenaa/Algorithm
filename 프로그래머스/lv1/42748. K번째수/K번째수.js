f3ea5ed77a85f53198881be3268cba717c03c31b
lsenaa <114736992+lsenaa@users.noreply.github.com>
2023-05-09 21:46:22 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+    let temp = [];
+    let answer = [];
+    
+    for (let i = 0; i < commands.length; i++) {
+        temp = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => a - b)
+        answer.push(temp[commands[i][2] - 1])
+    }
+    return answer
+}
+
+
+
